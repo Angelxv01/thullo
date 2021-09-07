00af42aed873825f199ff0b89c89faaebeab8bee
Angel <angeloxv@outlook.it>
2021-09-07 22:48:31 +0200

    additional changes to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,45 +1,46 @@
-const path = require("path");
-const HtmlWebPackPlugin = require("html-webpack-plugin");
+/* eslint-disable @typescript-eslint/no-var-requires */
+const path = require('path');
+const HtmlWebPackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: [path.join(__dirname, "client", "index.tsx"), "@babel/polyfill"],
+  entry: [path.join(__dirname, 'client', 'index.tsx'), '@babel/polyfill'],
   module: {
     rules: [
       {
         test: /\.tsx?$/,
-        use: "ts-loader",
+        use: 'ts-loader',
         exclude: /node_modules/,
       },
       {
         test: /\.js$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         options: {
-          presets: ["@babel/preset-react", "@babel/preset-env"],
+          presets: ['@babel/preset-react', '@babel/preset-env'],
         },
       },
       {
         test: /\.css$/,
-        use: ["style-loader", "css-loader"],
+        use: ['style-loader', 'css-loader'],
       },
     ],
   },
   resolve: {
-    extensions: [".tsx", ".ts", ".js"],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   devServer: {
-    static: path.resolve(__dirname, "build", "client"),
+    static: path.resolve(__dirname, 'build', 'client'),
     compress: true,
     port: 3000,
   },
-  devtool: "source-map",
+  devtool: 'source-map',
   output: {
-    filename: "main.js",
-    path: path.resolve(__dirname, "build", "client"),
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'build', 'client'),
   },
   plugins: [
     new HtmlWebPackPlugin({
-      template: "./public/index.html",
-      filename: "./index.html",
+      template: './public/index.html',
+      filename: './index.html',
     }),
   ],
 };
