821ff9c2969c9f41adb48b63666269a982141e90
Angel <angeloxv@outlook.it>
2021-11-02 23:50:39 +0100

    Reset Apollo server

--- server/index.ts
+++ server/index.ts
@@ -1,28 +1,30 @@
-import {ApolloServer} from 'apollo-server';
+import {ApolloServer, gql} from 'apollo-server';
 import {connect} from 'mongoose';
 import jwt from 'jsonwebtoken';
 
 import Logger from '../utils/Logger';
 import config from '../utils/config';
-import schema from './graphql/schema';
-import User, {IUser} from './models/User';
-import createDataLoader from './utils/createDataLoaders';
+// import schema from './graphql/schema';
+import User from './models/User';
+import { IUser } from '../types';
+// import createDataLoader from './utils/createDataLoaders';
 
 connect(config.MONGODB || '')
   .then(() => Logger.info('connected to MongoDB'))
   .catch(err => Logger.error(err));
 
 const server = new ApolloServer({
-  schema,
+  typeDefs: [gql`type Query{root: String}`],
+  // schema,
   context: async ({req}) => {
     const auth = req ? req.headers.authorization : null;
-    const dataLoader = createDataLoader();
+    // const dataLoader = createDataLoader();
     if (auth && auth.toLowerCase().startsWith('bearer ')) {
       const decoded = jwt.verify(auth.substr(7), config.SECRET) as {id: string};
       const currentUser: IUser = (await User.findById(decoded.id)) as IUser;
-      return {currentUser, dataLoader};
+      return {currentUser};
     }
-    return {dataLoader};
+    // return {dataLoader};
   },
 });
 void server.listen().then(({url}) => Logger.info(`Server ready at ${url}`));
