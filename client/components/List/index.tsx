23b96e3f234ca533b97861ef79717c87573e959c
angelxv01 <angeloxv@outlook.it>
2021-11-25 12:37:08 +0100

    Minor styling adjustment

--- client/components/List/index.tsx
+++ client/components/List/index.tsx
@@ -1,10 +1,9 @@
 import React, {useState} from 'react';
-import {useTheme} from 'styled-components';
 import * as GqlTypes from '../../../server/graphql/type';
-import useTextArea from '../../hooks/useTextArea';
 import Card from '../Card';
-import {Button, Flow, Icon, TextArea, Text} from '../common';
+import {Button, Flow, Icon} from '../common';
 import Header from './Header';
+import NewCard from './NewCard';
 import StyledList from './StyledList';
 
 const List = ({
@@ -31,33 +30,6 @@ const List = ({
         </Button.IconColored>
       </Flow>
     </StyledList>
-  );
-};
-
-const NewCard = () => {
-  const theme = useTheme();
-  const controller = useTextArea();
-  return (
-    <Flow
-      style={{
-        backgroundColor: `hsl(${theme.color.WHITE})`,
-        border: `1px solid hsl(${theme.color.GRAY5})`,
-        borderRadius: theme.border.radius[2],
-        boxShadow: '0px 2px 8px rgba(0, 0, 0, 0.1)',
-        padding: '1em',
-        maxWidth: '100%',
-      }}
-    >
-      <TextArea {...controller} placeholder="Enter a title for this card..." />
-      <Button.Colored backgroundColor="GREEN1" style={{padding: '0.4em 1em'}}>
-        <Text
-          fontFamily={theme.font.family.secondary}
-          fontSize={theme.font.size[200]}
-        >
-          Save
-        </Text>
-      </Button.Colored>
-    </Flow>
   );
 };
 
