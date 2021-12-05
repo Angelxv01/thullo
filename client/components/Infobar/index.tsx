566ed14604920661d20dac0bf274280c0d956d09
angelxv01 <angeloxv@outlook.it>
2021-12-05 18:47:54 +0100

    Refactor Visibility Modal

--- client/components/Infobar/index.tsx
+++ client/components/Infobar/index.tsx
@@ -6,24 +6,19 @@ import {useQuery} from '@apollo/client';
 import {Data, MASTER, Var} from '../../query';
 import {Button, Flex, Icon, Relative, Text} from '../common';
 
-import VisibilityBadge from './Badge';
 import Avatars from '../Avatars';
 import StyledInfobar from './StyledInfobar';
 import * as GQLTypes from '../../../server/graphql/type';
-import Toggle from '../Toggle';
-import {useTheme} from 'styled-components';
-import VisibilityCard from '../VisibilityCard';
 import Menu from '../Menu';
 import useVisibility from '../../hooks/useVisiblity';
 import InviteFriendModal from './InviteFriendModal';
+import VisibilityModal from './VisibilityModal';
 
 const Infobar = () => {
   const ctx = useQuery<Data, Var>(MASTER, {
     fetchPolicy: 'cache-only',
     variables: {id: '6182d8c9bba2b2dfab68119d'},
   });
-
-  const theme = useTheme();
 
   const [visibility, setVisibility] = useVisibility();
 
@@ -34,23 +29,7 @@ const Infobar = () => {
       <StyledInfobar>
         {/* Left hand side */}
         <Flex>
-          <Toggle
-            props={{
-              style: {
-                marginTop: '1em',
-                backgroundColor: 'white',
-                zIndex: theme.z.POPUP,
-                minWidth: '20em',
-                padding: '1em',
-                border: '1px solid #E0E0E0',
-                boxShadow: '0px 2px 4px rgba(0, 0, 0, 0.05)',
-                borderRadius: theme.border.radius[2],
-              },
-            }}
-          >
-            <VisibilityBadge visibility={ctx.data.board.visibility} />
-            <VisibilityCard />
-          </Toggle>
+          <VisibilityModal visibility={ctx.data.board.visibility} />
           <Avatars
             members={ctx.data.board.members.map(
               ({user}: {user: GQLTypes.User}) => user
