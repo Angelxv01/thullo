0936f97d7c8e690f1a4ef20747347bba55c32c5b
angelxv01 <angeloxv@outlook.it>
2021-12-15 12:37:55 +0100

    Finish the change

--- client/components/Infobar/StyledInfobar.ts
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export default styled.div`
-  display: flex;
-  padding: 1.5em 2em;
-  justify-content: space-between;
-`;
--- client/components/ListOperation/StyledListOperation.tsx
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-import {Flex} from '../common';
-
-export default styled(Flex)`
-  flex-direction: column;
-  justify-content: space-between;
-`;
