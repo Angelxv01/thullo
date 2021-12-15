ff73118f25cd431aa874429e0750494dbdf42056
angelxv01 <angeloxv@outlook.it>
2021-12-15 11:28:54 +0100

    Other bug fixes

--- cra-client/src/style/Icons.tsx
+++ cra-client/src/style/Icons.tsx
@@ -1,80 +1,112 @@
 import React, { HtmlHTMLAttributes } from "react";
-export const Apps = (props: HtmlHTMLAttributes<HTMLSpanElement>) => (
+export const Apps = (
+  props: HtmlHTMLAttributes<HTMLSpanElement>
+): JSX.Element => (
   <span className="material-icons" {...props}>
     &#xe5c3;
   </span>
 );
-export const Lock = (props: HtmlHTMLAttributes<HTMLSpanElement>) => (
+export const Lock = (
+  props: HtmlHTMLAttributes<HTMLSpanElement>
+): JSX.Element => (
   <span className="material-icons" {...props}>
     &#xe897;
   </span>
 );
-export const MoreHoriz = (props: HtmlHTMLAttributes<HTMLSpanElement>) => (
+export const MoreHoriz = (
+  props: HtmlHTMLAttributes<HTMLSpanElement>
+): JSX.Element => (
   <span className="material-icons" {...props}>
     &#xe5d3;
   </span>
 );
-export const Public = (props: HtmlHTMLAttributes<HTMLSpanElement>) => (
+export const Public = (
+  props: HtmlHTMLAttributes<HTMLSpanElement>
+): JSX.Element => (
   <span className="material-icons" {...props}>
     &#xe80b;
   </span>
 );
-export const ArrowDropDown = (props: HtmlHTMLAttributes<HTMLSpanElement>) => (
+export const ArrowDropDown = (
+  props: HtmlHTMLAttributes<HTMLSpanElement>
+): JSX.Element => (
   <span className="material-icons" {...props}>
     &#xe5c5;
   </span>
 );
-export const Add = (props: HtmlHTMLAttributes<HTMLSpanElement>) => (
+export const Add = (
+  props: HtmlHTMLAttributes<HTMLSpanElement>
+): JSX.Element => (
   <span className="material-icons" {...props}>
     &#xe145;
   </span>
 );
-export const Close = (props: HtmlHTMLAttributes<HTMLSpanElement>) => (
+export const Close = (
+  props: HtmlHTMLAttributes<HTMLSpanElement>
+): JSX.Element => (
   <span className="material-icons" {...props}>
     &#xe5cd;
   </span>
 );
-export const AttachFile = (props: HtmlHTMLAttributes<HTMLSpanElement>) => (
+export const AttachFile = (
+  props: HtmlHTMLAttributes<HTMLSpanElement>
+): JSX.Element => (
   <span className="material-icons" {...props}>
     &#xe226;
   </span>
 );
-export const Comment = (props: HtmlHTMLAttributes<HTMLSpanElement>) => (
+export const Comment = (
+  props: HtmlHTMLAttributes<HTMLSpanElement>
+): JSX.Element => (
   <span className="material-icons" {...props}>
     &#xe0b9;
   </span>
 );
-export const AccountCircle = (props: HtmlHTMLAttributes<HTMLSpanElement>) => (
+export const AccountCircle = (
+  props: HtmlHTMLAttributes<HTMLSpanElement>
+): JSX.Element => (
   <span className="material-icons" {...props}>
     &#xe853;
   </span>
 );
-export const Description = (props: HtmlHTMLAttributes<HTMLSpanElement>) => (
+export const Description = (
+  props: HtmlHTMLAttributes<HTMLSpanElement>
+): JSX.Element => (
   <span className="material-icons" {...props}>
     &#xe873;
   </span>
 );
-export const Edit = (props: HtmlHTMLAttributes<HTMLSpanElement>) => (
+export const Edit = (
+  props: HtmlHTMLAttributes<HTMLSpanElement>
+): JSX.Element => (
   <span className="material-icons" {...props}>
     &#xe3c9;
   </span>
 );
-export const Search = (props: HtmlHTMLAttributes<HTMLSpanElement>) => (
+export const Search = (
+  props: HtmlHTMLAttributes<HTMLSpanElement>
+): JSX.Element => (
   <span className="material-icons" {...props}>
     &#xe8b6;
   </span>
 );
-export const People = (props: HtmlHTMLAttributes<HTMLSpanElement>) => (
+export const People = (
+  props: HtmlHTMLAttributes<HTMLSpanElement>
+): JSX.Element => (
   <span className="material-icons" {...props}>
     &#xe7fb;
   </span>
 );
-export const Label = (props: HtmlHTMLAttributes<HTMLSpanElement>) => (
+export const Label = (
+  props: HtmlHTMLAttributes<HTMLSpanElement>
+): JSX.Element => (
   <span className="material-icons" {...props}>
     &#xe7fb;
   </span>
 );
-export const Image = (props: HtmlHTMLAttributes<HTMLSpanElement>) => (
+export const Image = (
+  props: HtmlHTMLAttributes<HTMLSpanElement>
+): JSX.Element => (
   <span className="material-icons" {...props}>
     &#xe3f4;
   </span>
