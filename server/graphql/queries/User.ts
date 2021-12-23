30d91578cb8753bbfb2ea324c5dcb80df1c9ac12
angelxv01 <angeloxv@outlook.it>
2021-12-23 08:49:24 +0100

    Added friendsNotInBoard query

--- server/graphql/queries/User.ts
+++ server/graphql/queries/User.ts
@@ -1,11 +1,18 @@
-import { gql } from 'apollo-server';
-import { IUser, UserDocument } from '../../../types';
+import { ApolloError, gql } from 'apollo-server';
+import { ObjectId } from 'mongoose';
+import { Context } from '../..';
+import { BoardDocument, IUser, UserDocument } from '../../../types';
 import User from '../../models/User';
+
+interface FriendsNotInBoardArgs {
+  id: ObjectId;
+}
 
 const typeDefs = gql`
   extend type Query {
     allUser: [User!]!
     authorizedUser: User
+    friendsNotInBoard(id: ID): [User!]!
   }
 `;
 
@@ -20,6 +27,25 @@ const resolvers = {
       _args: never,
       context: { currentUser: IUser }
     ) => context.currentUser,
+    friendsNotInBoard: async (
+      _: never,
+      args: FriendsNotInBoardArgs,
+      ctx: Context
+    ) => {
+      if (!ctx.currentUser) throw new ApolloError('Logged User Only');
+      const userFriends = ctx.currentUser.friends;
+      const currentBoard = (await ctx.dataLoader.BoardLoader.load(
+        args.id
+      )) as BoardDocument;
+      const boardMemberIds = currentBoard.members.map(member =>
+        String(member.id)
+      );
+      const friendsNotInBoard = userFriends.filter(
+        friend => boardMemberIds.indexOf(String(friend)) === -1
+      );
+
+      return ctx.dataLoader.UserLoader.loadMany(friendsNotInBoard);
+    },
   },
 };
 
