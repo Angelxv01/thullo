167ce23c33193260b0db5673a4105b72410a6309
angelxv01 <angeloxv@outlook.it>
2021-12-28 17:24:35 +0100

    general fixing

--- utils/Logger.ts
+++
@@ -1,5 +0,0 @@
-/* eslint-disable no-console */
-const info = (...args: [unknown]): void => console.log(args);
-const error = (...args: [unknown]): void => console.error(args);
-
-export default { info, error };
