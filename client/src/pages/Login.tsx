e2aa01f4876a19b0407df296309838e70e8a79ef
angelxv01 <angeloxv@outlook.it>
2022-04-20 00:24:08 +0200

    refactor login page

--- client/src/pages/Login.tsx
+++ client/src/pages/Login.tsx
@@ -9,20 +9,6 @@ import { saveState } from "../utils/localStorage";
 import { useNavigate } from "react-router-dom";
 import { Container } from "../style/Utils";
 
-export const LoginContainer = styled(Flow)`
-  margin: 10em auto;
-  width: min(25em, 30vw);
-  aspect-ratio: 4/5;
-  padding: 3em 0;
-  border-radius: ${({ theme }) => theme.border.radius[1]};
-  background-color: hsl(${({ theme }) => theme.color.WHITE});
-  display: flex;
-  justify-content: center;
-  flex-direction: column;
-  align-items: center;
-  font-size: ${({ theme }) => theme.font.size[400]};
-`;
-
 const Grid = styled.div`
   display: grid;
   grid-template-columns: repeat(2, auto);
@@ -74,40 +60,42 @@ const Login = () => {
 
   const theme = useTheme();
   return (
-    <Container>
-      <Logo />
-      <Flow className="header" style={{ textAlign: "center", width: "80%" }}>
-        <Text
-          fontSize={theme.font.size[500]}
-          fontFamily={theme.font.family.secondary}
-        >
-          Join thousands of learners from around the world{" "}
-        </Text>
-        <Text fontWeight="bold">Login to create your first project!</Text>
-        {error && (
-          <Button.Colored
-            backgroundColor="RED"
-            style={{
-              width: "100%",
-              padding: "0.5em",
-            }}
+    <Container className="h-full flex justify-center items-center">
+      <div className="w-96 space-y-8">
+        <Logo />
+        <Flow className="header" style={{ textAlign: "center", width: "80%" }}>
+          <Text
+            fontSize={theme.font.size[500]}
+            fontFamily={theme.font.family.secondary}
           >
-            Invalid credentials
-          </Button.Colored>
-        )}
-      </Flow>
-      <Grid>
-        <Text>Username</Text>
-        <input {...usernameController} placeholder="Username" />
-        <Text>Password</Text>
-        <input {...passwordController} placeholder="Password" />
-      </Grid>
-      <Button.Colored
-        style={{ padding: "0.5em 1em", fontSize: theme.font.size[400] }}
-        onClick={loginHandler}
-      >
-        Login
-      </Button.Colored>
+            Join thousands of learners from around the world
+          </Text>
+          <Text fontWeight="bold">Login to create your first project!</Text>
+          {error && (
+            <Button.Colored
+              backgroundColor="RED"
+              style={{
+                width: "100%",
+                padding: "0.5em",
+              }}
+            >
+              Invalid credentials
+            </Button.Colored>
+          )}
+        </Flow>
+        <Grid>
+          <Text>Username</Text>
+          <input {...usernameController} placeholder="Username" />
+          <Text>Password</Text>
+          <input {...passwordController} placeholder="Password" />
+        </Grid>
+        <Button.Colored
+          style={{ padding: "0.5em 1em", fontSize: theme.font.size[400] }}
+          onClick={loginHandler}
+        >
+          Login
+        </Button.Colored>
+      </div>
     </Container>
   );
 };
