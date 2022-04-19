8f2f49663117feb80f40a9d6319f252465239edb
angelxv01 <angeloxv@outlook.it>
2022-04-19 23:58:43 +0200

    refactor avatar

--- client/src/components/common/Avatar.tsx
+++ client/src/components/common/Avatar.tsx
@@ -1,28 +1,22 @@
 import React from "react";
-import styled from "styled-components";
 import { getAbbreviation } from "../../utils/formatting";
 
-const StyledAvatar = styled.img`
-  width: 2rem;
-  aspect-ratio: 1;
-  border-radius: ${({ theme }) => theme.border.radius[1]};
-  cursor: pointer;
-`;
+const TextAvatar = ({ children }: { children: React.ReactChild }) => (
+  <div className="rounded-lg cursor-pointer bg-gray-400 w-10 aspect-square flex justify-center items-center font-semibold">
+    {children}
+  </div>
+);
 
-const TextAvatar = styled.div`
-  width: 2rem;
-  aspect-ratio: 1;
-  display: grid;
-  place-items: center;
-  color: hsl(${({ theme }) => theme.color.WHITE});
-  background-color: hsl(${({ theme }) => theme.color.GRAY4});
-  border-radius: ${({ theme }) => theme.border.radius[1]};
-  cursor: pointer;
-`;
+const ImageAvatar = (props: React.ImgHTMLAttributes<HTMLImageElement>) => (
+  <img
+    className="rounded-lg object-cover object-center w-10 aspect-square"
+    {...props}
+  />
+);
 
 const Avatar = ({ id, username }: { id?: string; username: string }) => {
   if (!id) return <TextAvatar>{getAbbreviation(username)}</TextAvatar>;
-  return <StyledAvatar src={`https://source.unsplash.com/${id}/150x150`} />;
+  return <ImageAvatar src={`https://source.unsplash.com/${id}/150x150`} />;
 };
 
 export default Avatar;
