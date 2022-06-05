0123b8dc05c6796d6d97b0d907c6fd52f5315ea3
angelxv01 <angeloxv@outlook.it>
2022-06-05 17:24:58 +0200

    resets

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -2,27 +2,8 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     fontFamily: {
-      sans: "Poppins",
+      sans: ["'Poppins'", "sans-serif"],
       accent: "Noto Sans",
-    },
-    extend: {
-      fontSize: {
-        xs: ".625rem",
-        sm: ".75rem",
-        tiny: ".875rem",
-      },
-      colors: {
-        dark: "#000",
-        white: "#FFF",
-        green: {
-          dark: "#219653",
-          light: "#6FCF97",
-        },
-        yellow: "#F2C94C",
-        orange: "#F2994A",
-        red: "#EB5757",
-        blue: { dark: "#2F80ED", light: "#56CCF2" },
-      },
     },
   },
   // eslint-disable-next-line @typescript-eslint/no-var-requires
